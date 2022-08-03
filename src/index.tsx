c64e50758c13176d9e9f8b193d6faccc85af3925
Hironao OTSUBO <motemen@gmail.com>
2022-08-04 00:40:58 +0900

    jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first scheduled worker.
- *
- * - Run `wrangler dev --local` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/cdn-cgi/mf/scheduled"` to trigger the scheduled event
- * - Go back to the console to see what your worker has logged
- * - Update the Cron trigger in wrangler.toml (see https://developers.cloudflare.com/workers/wrangler/configuration/#triggers)
- * - Run `wrangler publish --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
- */
-
 import { SITES } from "./sites";
 import { EventTypeOf } from "./sites/base";
 import ical, { ICalCalendar } from "ical-generator";
-import { encode } from "html-entities";
+import h from "vhtml";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Fragment = ({ children }: { children: string[] }) =>
+  h(null as unknown as string, null, ...children);
 
 export interface Env {
   STORE: KVNamespace;
@@ -35,52 +27,74 @@ async function buildIndexHTML(kv: KVNamespace): Promise<string> {
     })
   );
 
-  return `<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>投稿短歌カレンダー</title>
-</head>
-<body>
-  <h1>投稿短歌カレンダー</h1>
-  <p>インターネットで投稿できる短歌の締め切りを確認できるカレンダーです。</p>
-  <p>現在、以下のサイトに対応しています:</p>
-  <ul>
-  ${crawlResults
-    .map(
-      ({ site, events }) => `
-      <li>
-        <strong><a href="${encode(site.homepage)}" target="_blank">${encode(
-        site.name
-      )}</a></strong>
+  const html = (
+    <html lang="ja">
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>投稿短歌カレンダー</title>
+      </head>
+      <body>
+        <h1>投稿短歌カレンダー</h1>
+        <p>ウェブで投稿できる短歌の締め切りを確認できるカレンダーです。</p>
+        <p>現在、以下のサイトに対応しています:</p>
         <ul>
-          ${events
-            .map((ev) => ({ date: ev.date, ...site.eventDetail(ev) }))
-            .map(
-              ({ date, title, url }) => `
+          {crawlResults.map(({ site, events }) => (
             <li>
-              <a href="${encode(url)}" target="_blank">${encode(title)}</a>
-              ${date[0]}/${date[1]}/${date[2]}
+              <strong>
+                <a href={site.homepage} target="_blank">
+                  {site.name}
+                </a>
+              </strong>
+              <ul>
+                {events
+                  .map((ev) => ({ date: ev.date, ...site.eventDetail(ev) }))
+                  .map(({ date, title, url }) => (
+                    <li>
+                      <a href={url} target="_blank">
+                        {title}
+                      </a>{" "}
+                      {date[0]}/{date[1]}/{date[2]}
+                    </li>
+                  ))}
+              </ul>
             </li>
-          `
-            )
-            .join("\n")}
+          ))}
         </ul>
-      </li>
-    `
-    )
-    .join("\n")}
-  </ul>
-  <p>以下のURLをカレンダーアプリなどから利用してください。</p>
-  <p><code>https://tanka-form-calendar.motemen.workers.dev/calendar.ics</code></p>
-  <p><iframe src="https://calendar.google.com/calendar/u/0/embed?src=0rfhaeq9n91cr7bt89ddbgfco1oeaqoq@import.calendar.google.com&amp;ctz=Asia/Tokyo" style="border: 0; max-width: 100%;" width="800" height="600" frameborder="0" scrolling="no"></iframe></p>
-  <footer>
-    <address>Author: <a href="https://twitter.com/@motemen" target="_blank">@motemen</a></address>
-    <a href="https://github.com/motemen/tanka-form-calendar/issues/new" target="_blank">ご意見ご要望</a>
-  </footer>
-</body>
-</html>
-`;
+        <p>以下のURLをカレンダーアプリなどから利用してください。</p>
+        <p>
+          <code>
+            https://tanka-form-calendar.motemen.workers.dev/calendar.ics
+          </code>
+        </p>
+        <p>
+          <iframe
+            src="https://calendar.google.com/calendar/u/0/embed?src=0rfhaeq9n91cr7bt89ddbgfco1oeaqoq@import.calendar.google.com&ctz=Asia/Tokyo"
+            style="border: 0; max-width: 100%;"
+            width="800"
+            height="600"
+            frameborder="0"
+            scrolling="no"
+          ></iframe>
+        </p>
+        <footer>
+          <address>
+            Author:{" "}
+            <a href="https://twitter.com/@motemen" target="_blank">
+              @motemen
+            </a>
+          </address>
+          <a
+            href="https://github.com/motemen/tanka-form-calendar"
+            target="_blank"
+          >
+            Source
+          </a>
+        </footer>
+      </body>
+    </html>
+  );
+
+  return `<!DOCTYPE html>${html}`;
 }
 
 async function buildICal(kv: KVNamespace): Promise<ICalCalendar> {
